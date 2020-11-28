848b908f7e84e21e5f608f06168b41d6a3b4b4ec
Jose Nuñez <jos.nuneza@gmail.com>
2020-11-28 16:22:43 -0300

    Clase 28-11

---
+++ 28-11/formulario_contacto/contacto/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Contacto(models.Model):
+    titulo = models.CharField(max_length=100)
+    mensaje = models.TextField()
+    receptor = models.CharField(max_length=100)
+    estado = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '#'+str(self.id)+' '+self.titulo + ' ('+ self.receptor+')'
